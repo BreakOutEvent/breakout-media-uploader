00e13b52a97aab1cba984793c554370a2e8747ca
Florian Schmidt <florian.schmidt.1994@icloud.com>
2017-04-25 15:22:18 +0200

    Add eslint, .eslintrc and fix some errors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var fs = require('fs');
-var express = require('express');
-var jsonwebtoken = require('jsonwebtoken');
-var app = express();
-var multer = require('multer');
-var uuid = require('node-uuid');
-var mime = require('mime');
+const fs = require('fs');
+const express = require('express');
+const jsonwebtoken = require('jsonwebtoken');
+const app = express();
+const multer = require('multer');
+const uuid = require('node-uuid');
+const mime = require('mime');
 
 const winston = require('winston');
 const logger = new (winston.Logger)({
@@ -19,11 +19,11 @@ const handler  = {
         logger.debug(`Accessing config key: ${key}`);
 
         if(process.env[key.toUpperCase()]) {
-          logger.debug(`Loading config key '${key.toUpperCase()}' from process.env`);
-          return process.env[key.toUpperCase()];
+            logger.debug(`Loading config key '${key.toUpperCase()}' from process.env`);
+            return process.env[key.toUpperCase()];
         } else if(target[key]) {
-          logger.debug(`Loading config key '${key}' from config.json`);
-          return target[key];
+            logger.debug(`Loading config key '${key}' from config.json`);
+            return target[key];
         } else {
             throw Error(`Neither process.env nor config.json contain key ${key}`);
         }
@@ -34,16 +34,16 @@ const target = require('./config.json');
 
 const config = new Proxy(target, handler);
 
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, config.uploadfolder)
+        cb(null, config.uploadfolder);
     },
     filename: function (req, file, cb) {
         cb(null, `${uuid.v4()}.${mime.extension(file.mimetype)}`);
     }
 });
 
-var upload = multer({storage: storage});
+const upload = multer({storage: storage});
 
 app.get('/', function (req, res) {
     res.status(200);
@@ -51,7 +51,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', upload.single('file'), auth, function (req, res) {
-    var tofile = `${config.movefoler}${req.body.id}###${req.file.originalname}`;
+    const tofile = `${config.movefoler}${req.body.id}###${req.file.originalname}`;
     fs.rename(req.file.path, tofile, function (err) {
         if (err) {
             logger.error(err);
@@ -67,9 +67,9 @@ app.post('/', upload.single('file'), auth, function (req, res) {
 
 function auth(req, res, next) {
     if (req.get('X-UPLOAD-TOKEN') && req.body.id && !isNaN(parseInt(req.body.id))) {
-        var token = req.get('X-UPLOAD-TOKEN');
-        var id = parseInt(req.body.id);
-        var decoded = parseInt(jsonwebtoken.decode(token, config.jwt_secret, 'HS512').subject);
+        const token = req.get('X-UPLOAD-TOKEN');
+        const id = parseInt(req.body.id);
+        const decoded = parseInt(jsonwebtoken.decode(token, config.jwt_secret, 'HS512').subject);
         if (decoded === id) {
             logger.info(`auth succeeded for id: ${id}`);
             next();
@@ -86,9 +86,9 @@ function auth(req, res, next) {
 
 }
 
-var server = app.listen(3001, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen(3001, function () {
+    const host = server.address().address;
+    const port = server.address().port;
 
     logger.info('Media-uploader listening at http://%s:%s', host, port);
 });
